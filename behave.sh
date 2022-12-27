9125d6f59b421456415f144d1a8e507c5ce0ca9f
SANMATHI07 <115545050+SANMATHI07@users.noreply.github.com>
2022-12-27 14:39:12 +0530

    Update behave.sh

--- behave.sh
+++ behave.sh
@@ -19,7 +19,7 @@ behave_install()
 cleanup_workspace()
 {
        echo "removing old behave files"
-       sudo rm -r /var/lib/jenkins/workspace/newbehave
+       sudo rm -r /var/lib/jenkins/workspace/behave
        if [ $? -ne 0 ]; then
                 echo "error while removing behave files"
        fi
@@ -28,10 +28,10 @@ cleanup_workspace()
 behave_steps()
 {
         echo "steps to run BDD"
-        sudo mkdir -p /var/lib/jenkins/workspace/newbehave/features/steps
-        sudo cp -r ./test.feature /var/lib/jenkins/workspace/newbehave/features
-        sudo cp -r ./test.py /var/lib/jenkins/workspace/newbehave/features/steps
-        cd /var/lib/jenkins/workspace/newbehave
+        sudo mkdir -p /var/lib/jenkins/workspace/behave/features/steps
+        sudo cp -r ./test.feature /var/lib/jenkins/workspace/behave/features
+        sudo cp -r ./test.py /var/lib/jenkins/workspace/behave/features/steps
+        cd /var/lib/jenkins/workspace/behave
         behave
               
 }
